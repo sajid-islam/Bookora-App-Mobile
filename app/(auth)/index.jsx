84231c919f22c3a0d1847698f4c2d7b9e8e4e95b
sajid-islam <sajidislam941@gmail.com>
2025-07-18 22:00:33 +0600

    complete design login page

---
+++ app/(auth)/index.jsx
@@ -0,0 +1,130 @@
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    ActivityIndicator,
+    KeyboardAvoidingView,
+    Platform,
+} from "react-native";
+import React, { useState } from "react";
+import styles from "./../../assets/styles/login.style.js";
+import { Image } from "expo-image";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/color.js";
+import { Link } from "expo-router";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setLoading] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleLogin = () => {};
+
+    return (
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <View style={styles.container}>
+                <View style={styles.illustrationImage}>
+                    <Image
+                        style={styles.illustrationImage}
+                        source={require("../../assets/images/book-bro.png")}
+                        contentFit="contain"
+                    />
+                </View>
+                <View style={styles.card}>
+                    <View>
+                        {/* Email */}
+                        <View style={styles.inputGroup}>
+                            <Text style={styles.label}>Email</Text>
+                            <View style={styles.inputContainer}>
+                                <Ionicons
+                                    name="mail-outline"
+                                    size={20}
+                                    color={COLORS.primary}
+                                    style={styles.inputIcon}
+                                />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Enter you email"
+                                    placeholderTextColor={
+                                        COLORS.placeholderText
+                                    }
+                                    value={email}
+                                    onChangeText={setEmail}
+                                    keyboardType="email-address"
+                                    autoCapitalize="none"
+                                />
+                            </View>
+                        </View>
+                        {/* Password */}
+                        <View style={styles.inputGroup}>
+                            <Text style={styles.label}>Password</Text>
+                            <View style={styles.inputContainer}>
+                                <Ionicons
+                                    name="lock-closed-outline"
+                                    size={20}
+                                    color={COLORS.primary}
+                                    style={styles.inputIcon}
+                                />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="••••••••"
+                                    placeholderTextColor={
+                                        COLORS.placeholderText
+                                    }
+                                    value={password}
+                                    onChangeText={setPassword}
+                                    secureTextEntry={!showPassword}
+                                />
+                                <TouchableOpacity
+                                    onPress={() =>
+                                        setShowPassword(!showPassword)
+                                    }
+                                    style={styles.eyeIcon}
+                                >
+                                    <Ionicons
+                                        name={
+                                            showPassword
+                                                ? "eye-off-outline"
+                                                : "eye-outline"
+                                        }
+                                        size={20}
+                                        color={COLORS.primary}
+                                    />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+
+                    <TouchableOpacity
+                        style={styles.button}
+                        disabled={isLoading}
+                        onPress={handleLogin}
+                    >
+                        {isLoading ? (
+                            <ActivityIndicator color="#fff" />
+                        ) : (
+                            <Text style={styles.buttonText}>Login</Text>
+                        )}
+                    </TouchableOpacity>
+
+                    {/* Footer */}
+                    <View style={styles.footer}>
+                        <Text style={styles.footerText}>
+                            Do not have an account?{" "}
+                        </Text>
+                        <Link href="/(auth)/signup" asChild>
+                            <TouchableOpacity>
+                                <Text style={styles.link}>Signup</Text>
+                            </TouchableOpacity>
+                        </Link>
+                    </View>
+                </View>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
--- constants/color.js
+++ constants/color.js
@@ -1,4 +1,5 @@
 // FOREST
+/*
 const COLORS = {
     primary: "#4CAF50",
     textPrimary: "#2e5a2e",
@@ -12,7 +13,7 @@ const COLORS = {
     white: "#ffffff",
     black: "#000000",
 };
-
+*/
 // RETRO
 /*
 const COLORS = {
@@ -31,21 +32,20 @@ const COLORS = {
 */
 
 // OCEAN
-/*
+
 const COLORS = {
-  primary: "#1976D2", 
-  textPrimary: "#1a4971",
-  textSecondary: "#6d93b8", 
-  textDark: "#0d2b43", 
-  placeholderText: "#767676",
-  background: "#e3f2fd", 
-  cardBackground: "#f5f9ff",
-  inputBackground: "#f0f8ff", 
-  border: "#bbdefb",
-  white: "#ffffff",
-  black: "#000000",
+    primary: "#1976D2",
+    textPrimary: "#1a4971",
+    textSecondary: "#6d93b8",
+    textDark: "#0d2b43",
+    placeholderText: "#767676",
+    background: "#e3f2fd",
+    cardBackground: "#f5f9ff",
+    inputBackground: "#f0f8ff",
+    border: "#bbdefb",
+    white: "#ffffff",
+    black: "#000000",
 };
-*/
 
 // BLOSSOM
 /*
