30a92e6a519789eac7d00f16dfc289c158f56ca3
sajid-islam <sajidislam941@gmail.com>
2025-07-25 15:07:35 +0600

    implement tab navigation (home,create,profile)

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,35 @@
-import { Stack } from "expo-router";
+import { Slot, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import SafeScreen from "./../components/SafeScreen";
+import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
+import { useAuthStore } from "../store/authStore";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
+    const router = useRouter();
+    const segments = useSegments();
+    const { checkAuth, token, user } = useAuthStore();
+    const [isReady, setIsReady] = useState(false);
+
+    useEffect(() => {
+        checkAuth().finally(() => setIsReady(true));
+    }, [checkAuth]);
+
+    useEffect(() => {
+        if (!isReady) return;
+        const inAuthScreen = segments[0] === "(auth)";
+        const isSignedIn = user && token;
+
+        if (!inAuthScreen && !isSignedIn) router.replace("/(auth)");
+        else if (inAuthScreen && isSignedIn) router.replace("/(tabs)");
+    }, [user, token, segments, router, isReady]);
+
+    if (!isReady) return null;
+
     return (
         <SafeAreaProvider>
             <SafeScreen>
-                <Stack screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="index" />
-                    <Stack.Screen name="(auth)" />
-                </Stack>
+                <Slot />
             </SafeScreen>
             <StatusBar style="dark" />
         </SafeAreaProvider>
--- store/authStore.js
+++ store/authStore.js
@@ -43,7 +43,7 @@ export const useAuthStore = create((set) => ({
         set({ isLoading: true });
         try {
             const response = await fetch(
-                "http://192.168.0.103:3001/api/auth/login",
+                `http://${process.env.IP_ADDRESS}:3001/api/auth/login`,
                 {
                     method: "POST",
                     headers: {
