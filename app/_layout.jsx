a3a01337dc742c9a7db2bf600043fa6a64204e07
sajid-islam <sajidislam941@gmail.com>
2025-08-04 10:46:17 +0600

    Add jetBrainsmono font and change splash screen logo

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,27 @@
-import { Slot, useRouter, useSegments } from "expo-router";
+import { Slot, SplashScreen, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
 import { useAuthStore } from "../store/authStore";
 import { useEffect, useState } from "react";
+import { useFonts } from "expo-font";
 
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
     const router = useRouter();
     const segments = useSegments();
     const { checkAuth, token, user } = useAuthStore();
     const [isReady, setIsReady] = useState(false);
+
+    const [loaded, error] = useFonts({
+        "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
 
     useEffect(() => {
         checkAuth().finally(() => setIsReady(true));
